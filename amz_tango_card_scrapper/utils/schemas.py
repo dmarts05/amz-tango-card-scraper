e3c7bcf67874f11fbdbb839c6fea4e76d1e6a733
dmarts05 <dmarts05@estudiantes.unileon.es>
2023-05-14 21:29:31 +0200

    refactor(config): Make every config parameter mandatory
    Every config parameter must now be included in the config file even if it's not going to be used. If that's the case, leaving it blank won't have any effect, but it is mandatory to at least have the field present.

--- amz_tango_card_scrapper/utils/schemas.py
+++ amz_tango_card_scrapper/utils/schemas.py
@@ -1,27 +1,27 @@
-"""Module for defining types."""
+"""Module that contains the schemas used in the project."""
 
-from typing import Dict, List, NamedTuple, Optional
+from typing import Dict, List, NamedTuple
 
 
 class ConfigFile(NamedTuple):
     """
-    A custom type that encapsulates the structure of the config file.
+    A schema that encapsulates the structure of the config file.
 
     gmail: the Gmail section of the config file
-    amazon: the Amazon section of the config file (if present)
+    amazon: the Amazon section of the config file
     from_list: the From section of the config file
     script: the Script section of the config file
     """
 
     gmail: Dict[str, str]
-    amazon: Optional[Dict[str, str]]
+    amazon: Dict[str, str]
     from_list: List[str]
     script: Dict[str, bool]
 
 
 class TangoCard(NamedTuple):
     """
-    A custom type that represents a tango card.
+    A schema that represents a tango card.
 
     security_code: the security code of the tango card
     tango_link: the link that will be used to redeem the tango card
