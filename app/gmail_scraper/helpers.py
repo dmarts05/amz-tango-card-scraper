370ea1fe2bcd83cea409febd549cce875859ff52
dmarts05 <dmarts05@estudiantes.unileon.es>
2023-05-20 12:20:49 +0200

    refactor(gmail_scraper): Move aux functions to helpers.py

---
+++ app/gmail_scraper/helpers.py
@@ -0,0 +1,59 @@
+from email.message import Message
+
+from bs4 import BeautifulSoup
+
+from app.utils.schemas import TangoCard
+
+
+def get_body_of_email(msg: Message) -> str:  # type: ignore
+    """
+    Get the body of an email.
+
+    Args:
+        msg: Email message.
+
+    Returns:
+        Body of the email.
+    """
+    if msg.is_multipart():
+        for part in msg.walk():
+            ctype = part.get_content_type()
+            cdispo = str(part.get("Content-Disposition"))
+
+            # Skip any text/plain (txt) attachments
+            if ctype == "text/plain" and "attachment" not in cdispo:
+                return part.get_payload(decode=True).decode("utf-8")
+    else:
+        return msg.get_payload(decode=True).decode("utf-8")
+
+
+def extract_tango_card_from_body(body: str) -> TangoCard:
+    """
+    Extract Tango Card from the body of an email.
+
+    Args:
+        body: Body of the email.
+
+    Returns:
+        Extracted Tango Card.
+    """
+    from .constants import SECURITY_CODE_CLASS, TANGO_LINK_CLASS
+
+    soup = BeautifulSoup(body, "html.parser")
+
+    # Extract the security code
+    security_code = soup.find_all("div", {"class": SECURITY_CODE_CLASS})[3].get_text()
+
+    # Extract Tango link
+    tango_link = soup.find_all("div", {"class": TANGO_LINK_CLASS})[3].find("a").get("href")
+
+    # Extract Amazon link
+    amazon_link = (  # type: ignore
+        "https://www.amazon."
+        + body.split("http://www.amazon.", 1)[1].split(  # type: ignore
+            "/",
+            1,
+        )[0]
+    )
+
+    return TangoCard(security_code=security_code, tango_link=tango_link, amazon_link=amazon_link)  # type: ignore
