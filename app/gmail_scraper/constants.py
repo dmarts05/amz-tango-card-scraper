ab555936b0d0b8f4bfddf1cdc099115b9cc13d47
dmarts05 <dmarts05@estudiantes.unileon.es>
2023-05-20 12:15:50 +0200

    refactor(gmail_scraper): Add related constants

--- app/gmail_scraper/constants.py
+++ app/gmail_scraper/constants.py
@@ -1,5 +1,7 @@
 """Module containing constants for the Gmail scraper."""
 
 IMAP_GMAIL_URL = "imap.gmail.com"
-
 EMAIL_FORMAT = "(RFC822)"
+
+SECURITY_CODE_CLASS = "tango-credential-value"
+TANGO_LINK_CLASS = "tango-credential-key"
